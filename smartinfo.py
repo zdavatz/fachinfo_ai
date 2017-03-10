3ec3fde04d377047bb574ba32225fe52a2230759
cybermax <cybrmx@gmail.com>
2017-03-10 16:45:57 +0100

    - first part of the language-related refactoring - added command line options for "de" and "fr (--lang=de or lang=fr)

--- smartinfo.py
+++ smartinfo.py
@@ -13,6 +13,7 @@
 #
 
 import sys
+import getopt
 import sqlite3 as sql
 import nltk
 import string
@@ -34,6 +35,8 @@ chapter_ids = ["section1", "section2", "section3", "section4", "section5", "sect
                "section9", "section10", "section11", "section12", "section13", "section14", "section15", "section16",
                "Section7000", "Section7050", "Section7100", "Section7150", "Section7200", "Section7250", "Section7350",
                "Section7400", "Section7450", "Section7500", "Section7550", "Section7600", "Section7650", "Section7700"]
+
+list_of_stopwords = []
 
 
 def is_integer(s):
@@ -97,7 +100,7 @@ def get_tokens(text):
         # Remove the punctuation using the character deletion step of translate
         tokens = nltk.word_tokenize(text)
         tokens = multi_word_tokenizer.tokenize(tokens)
-        filtered = [w for w in tokens if w not in all_stopwords]
+        filtered = [w for w in tokens if w not in list_of_stopwords]
         filtered = [w for w in filtered if w not in string.punctuation]
         filtered = [item for item in filtered if not is_integer(item)]
         filtered = [word for word in filtered if len(word) >= 3]
@@ -105,7 +108,7 @@ def get_tokens(text):
     return []
 
 
-def clean_up_string(s):
+def clean_up_string(lang, s):
     """
     Cleans input string
     Note: the regexes are not ORed for clearity
@@ -117,8 +120,16 @@ def clean_up_string(s):
         for c in chars:
             if c in s:
                 s = s.replace(c, "")
-        # Remove all -fachen, -faches, etc
-        s = re.sub(r"[-]?[0-9]?(Fach(en|es|e)|fac(h|he|hen)|stündig(e|en)|wöchige(r|n)|monatig(e|en)|jährig(e|en))", "", s)
+
+        if lang == "de":
+            # Remove all -fachen, -faches, etc
+            s = re.sub(r"[-]?[0-9]?(Fach(en|es|e)|fac(h|he|hen)|stündig(e|en)|wöchige(r|n)|monatig(e|en)|jährig(e|en))", "", s)
+            # Remove special strings
+            s = s.replace("o.ä.", "").replace("z.B.", "").replace("’’", "")
+        elif lang == "fr":
+            # Remove all -fachen, -faches, etc
+            s = re.sub(r"[-]?[0-9]?(Fach(en|es|e)|fac(h|he|hen)|stündig(e|en)|wöchige(r|n)|monatig(e|en)|jährig(e|en))", "", s)
+
         # Remove numbers and dots before letters, e.g. 1.Drehen -> Drehen
         s = re.sub(r"^([0-9]+.|-|−)([a-zA-ZäöüèéàÜÖÄ]+)$", "\2", s)
         # Remove time with format hh:mm:ss
@@ -144,8 +155,6 @@ def clean_up_string(s):
             s = s[1:]
         # Remove all strings with this format (+/-)60**
         s = re.sub(r"^[+-−.]?[0-9]+\*+$", "", s)
-        # Remove special strings
-        s = s.replace("o.ä.", "").replace("z.B.", "").replace("’’", "")
         # Remove underscores _ from multi words tokenized text, e.g. Multiple_Sklerose
         s = s.replace("_", " ")
         # Remove all strings that are smaller than 3 chars
@@ -154,7 +163,7 @@ def clean_up_string(s):
     return s
 
 
-def find_chapters_with_tokens(soup, tokens):
+def find_chapters_with_tokens(soup, tokens, mw_set):
     """
     Given a soup object representing the "Fachinfo" and a list of tokens/words,
     extracts the ids of the chapters containing those words
@@ -163,9 +172,6 @@ def find_chapters_with_tokens(soup, tokens):
     :return: dictionary of the form word -> string (chapter1,chapter2,...)
     """
     word_to_chapter_dict = {}
-
-    # Note to myself: gotta love list comprehensions
-    mw_set = set([mw.strip() for mw in multi_words])
 
     # Extract all chapter ids
     divs = soup.find_all("div", id=lambda x: x and (x.startswith("section") or x.startswith("Section")))
@@ -195,151 +201,179 @@ def find_chapters_with_tokens(soup, tokens):
     return word_to_chapter_dict
 
 
-# Check if directories exist, otherwise generate them
-if not os.path.exists("./output"):
-    os.makedirs("./output")
+def main(argv):
+    # List of all relevant files
+    amiko_db_full_idx = {"de": "amiko_db_full_idx_de.db", "fr": "amiko_db_full_idx_fr.db"}
+    stopwords_file = {"de": "stopwords_de.txt", "fr": "stopwords_fr.txt"}
+    whitelist_file = {"de": "whitelist_de.txt", "fr": "whitelist_fr.txt"}
+    multiwords_file = {"de": "multiwords_de.txt", "fr": "multiwords_fr.txt"}
+    frequency_file = {"de": "frequency_de.csv", "fr": "frequency_fr.csv"}
+    auto_stopwords_file = {"de": "auto_stopwords_de.csv", "fr": "auto_stopwords_fr.csv"}
+    amiko_frequency_db = {"de": "amiko_frequency_de.db", "fr": "amiko_frequency_fr.db"}
 
+    # Check if directories exist, otherwise generate them
+    if not os.path.exists("./output"):
+        os.makedirs("./output")
 
-# Open connection to database for reading
-con = None
-rows = []
-try:
-    con = sql.connect("./dbs/amiko_db_full_idx_de.db")
-    cur = con.cursor()
-    # Retrieve all articles
-    query = "SELECT * FROM amikodb"
-    cur.execute(query)
-    rows = cur.fetchall()
-except sql.Error:
-    print("Error %s:" % sql.Error.args[0])
-    sys.exit(1)
-finally:
+    # Language flag
+    lang = "de"
+    try:
+        opts, args = getopt.getopt(argv, "hlang:")
+    except getopt.GetoptError:
+        print("smartinfo.py -lang <language>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            print("smartinfo.py -lang <language>")
+        if opt == "-lang":
+            lang = arg
+
+    # Open connection to database for reading
+    con = None
+    rows = []
+    try:
+        con = sql.connect("./dbs/" + amiko_db_full_idx[lang])
+        cur = con.cursor()
+        # Retrieve all articles
+        query = "SELECT * FROM amikodb"
+        cur.execute(query)
+        rows = cur.fetchall()
+    except sql.Error:
+        print("Error %s:" % sql.Error.args[0])
+        sys.exit(1)
+    finally:
+        if con:
+            con.close()
+
+    # Read our stop words
+    stop_words = []
+    with open("./input/" + stopwords_file[lang], encoding="utf-8") as file:
+        for line in file:
+            line = line.strip()
+            stop_words.append(line)
+
+    # Read our whitelist words
+    white_words = []
+    with open("./input/" + whitelist_file[lang], encoding="utf-8") as file:
+        for line in file:
+            line = line.strip()
+            white_words.append(line)
+
+    # Read our list of multi words
+    multi_words = []
+    with open("./input/" + multiwords_file[lang], encoding="utf-8") as file:
+        for line in file:
+            line.strip()
+            multi_words.append(line)
+
+    # Add multiwords to tokenizer
+    for mw in multi_words:
+        multi_word_tokenizer.add_mwe(tuple(mw.strip().split(" ")))  # Needs a tuple
+
+    # Open frequency file for write
+    csvfile = open("./output/" + frequency_file[lang], "w", newline="", encoding="utf-8")
+    wr = csv.writer(csvfile, quoting=csv.QUOTE_NONE, delimiter=';')
+
+    # Open stop_word file for write
+    auto_stopwords_file = open("./output/" + auto_stopwords_file[lang], "w", newline="", encoding="utf-8")
+    auto_stop_wr = csv.writer(auto_stopwords_file, quoting=csv.QUOTE_NONE, delimiter=";")
+
+    # All stop words
+    list_of_stopwords = set(stopwords.words("german")) | set(stop_words)
+
+    # Open connection to database for writing
+    # Format with three columns
+    #    id (primary key), keyword, regnr (chapter)
+    try:
+        con = sql.connect("./output/" + amiko_frequency_db[lang])
+        cur = con.cursor()
+        # Create a table with two columns
+        cur.execute("DROP TABLE IF EXISTS frequency")
+        cur.execute("CREATE TABLE frequency (id TEXT PRIMARY_KEY, keyword TEXT, regnr TEXT);")
+        con.commit()
+    except sql.Error:
+        sys.exit(1)
+
+    # Start big loop
+    start = time.time()
+    word_dict = {}  # Empty dictionary
+
+    # Note to myself: gotta love list comprehensions
+    mw_set = set([mw.strip() for mw in multi_words])
+
+    for i in range(0, 50):   # len(rows)):
+        title = rows[i][1]
+        title = title.replace(";", " ")
+        html_content = rows[i][15]
+
+        # Column 5: swissmedic number 5
+        # Column 15: html content
+        regnr = rows[i][5]
+        if regnr:
+            regnr = regnr.split(",")[0]
+
+        if regnr:   # regnr == "47311"
+            soup_object = remove_html_tags(html_content)
+            if soup_object:
+                clean_text = soup_object.get_text(separator=" ")
+                if clean_text:
+                    tokens = get_tokens(clean_text)
+                    # Note to myself: list comprehensions are cool!
+                    tokens = [clean_up_string(lang, t) for t in tokens]
+                    # Remove empty strings (note: filter retuns a filter object -> needs to be transformed to list)
+                    tokens = list(filter(None, tokens))
+                    # Get word count
+                    count = Counter(tokens)
+                    size = len(count)
+                    frequency_list = sorted(list(count.most_common(size)))
+
+                    # Dictionary of the form word -> string (chapter1,chapter2,...)
+                    w_to_c_dict = find_chapters_with_tokens(soup_object, tokens, mw_set)
+
+                    # Add to map
+                    for word in frequency_list:
+                        w = word[0]
+                        if w:
+                            ch_ids = ""
+                            if w in w_to_c_dict:
+                                ch_ids = "(" + w_to_c_dict[w] + ")"
+                            regnr_prime = regnr + ch_ids
+                            if w not in word_dict:
+                                word_dict[w] = regnr_prime
+                            else:
+                                updated_entry = word_dict[w] + "|" + regnr_prime
+                                word_dict[w] = updated_entry
+
+                    print(title, frequency_list)
+
+    print("\n============================================================\n")
+    cnt = 0
+    for k in sorted(word_dict):
+        r = word_dict[k]    # registration number swissmedic-5
+        # Change this number to increase or decrease the number of auto-generated stopwords
+        if k not in white_words and len(r.split(",")) > 400:
+            auto_stop_wr.writerow([k])
+        else:
+            cnt += 1
+            line = (k, r)       # word, registration numbers
+            wr.writerow(line)
+            if cnt % 100 == 0:
+                print("\rSaved: %d" % cnt, end='', flush=True)
+                con.commit()
+            # Generate 16 byte hash
+            hashed_k = hashlib.sha256(k.encode('utf-8')).hexdigest()[:10]
+            query = "INSERT INTO frequency VALUES('%s', '%s', '%s');" % (hashed_k, k, r)
+            con.execute(query)
+
     if con:
+        con.commit()
         con.close()
 
+    end = time.time()
 
-# Read our stop words
-stop_words = []
-with open("./input/stopwords.txt", encoding="utf-8") as file:
-    for line in file:
-        line = line.strip()
-        stop_words.append(line)
+    print("\n\n============================================================\n")
+    print("Elapsed time = %.3fs" % (end-start))
 
-# Read our whitelist words
-white_words = []
-with open("./input/whitelist.txt", encoding="utf-8") as file:
-    for line in file:
-        line = line.strip()
-        white_words.append(line)
-
-# Read our list of multi words
-multi_words = []
-with open("./input/multiwords.txt", encoding="utf-8") as file:
-    for line in file:
-        line.strip()
-        multi_words.append(line)
-
-# Add multiwords to tokenizer
-for mw in multi_words:
-    multi_word_tokenizer.add_mwe(tuple(mw.strip().split(" ")))  # Needs a tuple
-
-# All stop words
-all_stopwords = set(stopwords.words('german')) | set(stop_words)
-
-# Open frequency file for write
-csvfile = open("./output/frequency.csv", "w", newline="", encoding="utf-8")
-wr = csv.writer(csvfile, quoting=csv.QUOTE_NONE, delimiter=';')
-
-# Open stop_word file for write
-auto_stopwords_file = open("./output/auto_stopwords.csv", "w", newline="", encoding="utf-8")
-auto_stop_wr = csv.writer(auto_stopwords_file, quoting=csv.QUOTE_NONE, delimiter=";")
-
-# Open connection to database for writing
-# Format with three columns
-#    id (primary key), keyword, regnr (chapter)
-try:
-    con = sql.connect("./output/amiko_frequency.db")
-    cur = con.cursor()
-    # Create a table with two columns
-    cur.execute("DROP TABLE IF EXISTS frequency")
-    cur.execute("CREATE TABLE frequency (id TEXT PRIMARY_KEY, keyword TEXT, regnr TEXT);")
-    con.commit()
-except sql.Error:
-    sys.exit(1)
-
-# Start big loop
-start = time.time()
-word_dict = {}  # Empty dictionary
-
-for i in range(0, len(rows)):
-    title = rows[i][1]
-    title = title.replace(";", " ")
-    html_content = rows[i][15]
-
-    # Column 5: swissmedic number 5
-    # Column 15: html content
-    regnr = rows[i][5]
-    if regnr:
-        regnr = regnr.split(",")[0]
-
-    if regnr:   # == "32934":   # regnr == "47311"
-        soup_object = remove_html_tags(html_content)
-        if soup_object:
-            clean_text = soup_object.get_text(separator=" ")
-            if clean_text:
-                tokens = get_tokens(clean_text)
-                # Note to myself: list comprehensions are cool!
-                tokens = [clean_up_string(t) for t in tokens]
-                # Remove empty strings (note: filter retuns a filter object -> needs to be transformed to list)
-                tokens = list(filter(None, tokens))
-                # Get word count
-                count = Counter(tokens)
-                size = len(count)
-                frequency_list = sorted(list(count.most_common(size)))
-
-                # Dictionary of the form word -> string (chapter1,chapter2,...)
-                w_to_c_dict = find_chapters_with_tokens(soup_object, tokens)
-
-                # Add to map
-                for word in frequency_list:
-                    w = word[0]
-                    if w:
-                        ch_ids = ""
-                        if w in w_to_c_dict:
-                            ch_ids = "(" + w_to_c_dict[w] + ")"
-                        regnr_prime = regnr + ch_ids
-                        if w not in word_dict:
-                            word_dict[w] = regnr_prime
-                        else:
-                            updated_entry = word_dict[w] + "|" + regnr_prime
-                            word_dict[w] = updated_entry
-
-                print(title, frequency_list)
-
-print("\n============================================================\n")
-cnt = 0
-for k in sorted(word_dict):
-    r = word_dict[k]    # registration number swissmedic-5
-    # Change this number to increase or decrease the number of auto-generated stopwords
-    if k not in white_words and len(r.split(",")) > 400:
-        auto_stop_wr.writerow([k])
-    else:
-        cnt += 1
-        line = (k, r)       # word, registration numbers
-        wr.writerow(line)
-        if cnt % 100 == 0:
-            print("\rSaved: %d" % cnt, end='', flush=True)
-            con.commit()
-        # Generate 16 byte hash
-        hashed_k = hashlib.sha256(k.encode('utf-8')).hexdigest()[:10]
-        query = "INSERT INTO frequency VALUES('%s', '%s', '%s');" % (hashed_k, k, r)
-        con.execute(query)
-
-if con:
-    con.commit()
-    con.close()
-
-end = time.time()
-
-print("\n\n============================================================\n")
-print("Elapsed time = %.3fs" % (end-start))
+# MAIN starts here
+if __name__ == "__main__":
+    main(sys.argv[1:])
